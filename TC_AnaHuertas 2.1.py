28c37b47e844e6b7622db9fd1538be5b6c9b8c85
AnaHuertas <61410180+AnaHuertas@users.noreply.github.com>
2020-02-25 22:17:17 +0100

    Delete TC_AnaHuertas 2.1.py

--- TC_AnaHuertas 2.1.py
+++
@@ -1,7 +0,0 @@
-#with string interpolation!!!!!!!!!
-reciever = 'Maggie'
-sender = 'Gloria'
-timeframe = 'whenever'
-email1 = ['Hello ','! we will answer ',' This is ']
-print(f'Hello {reciever}! we will answer {timeframe} This is {sender}')
-print(f'{email1[0]}{reciever}{email1[1]}{timeframe}{email1[2]}{sender}')
