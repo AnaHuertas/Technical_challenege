a080ceb106fc807191c2123ae98ae08ff7d326ff
AnaHuertas <61410180+AnaHuertas@users.noreply.github.com>
2020-02-25 09:51:23 +0100

    Add files via upload

---
+++ TC_AnaHuertas 2.1.py
@@ -0,0 +1,7 @@
+#with string interpolation!!!!!!!!!
+reciever = 'Maggie'
+sender = 'Gloria'
+timeframe = 'whenever'
+email1 = ['Hello ','! we will answer ',' This is ']
+print(f'Hello {reciever}! we will answer {timeframe} This is {sender}')
+print(f'{email1[0]}{reciever}{email1[1]}{timeframe}{email1[2]}{sender}')
